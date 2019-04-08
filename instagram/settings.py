cd98410369675d9e55c330ef4661fd5be91f7bcb
Boyoun Park <lovelychoco100@gmail.com>
2019-04-08 05:39:25 +0000

    settings.py에 base.html이 있는 templates 폴더 위치 추가

--- instagram/settings.py
+++ instagram/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'instagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join()], # /Instagram/templates
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # /Instagram/templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
